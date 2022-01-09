17cb07599a51e1f0068c5246d18e5c3db4ebeaf7
Wiktor Religo <wiciorny@gmail.com>
2022-01-09 22:59:34 +0100

    set up JPA persistence and hibernate config

--- employee-service/src/main/java/org/jadevirek/configuration/database/H2DataBaseConfiguration.java
+++ employee-service/src/main/java/org/jadevirek/configuration/database/H2DataBaseConfiguration.java
@@ -1,10 +1,59 @@
 package org.jadevirek.configuration.database;
 
 
+import org.h2.tools.Server;
 import org.jadevirek.configuration.annotations.DataConfiguration;
 
+import javax.persistence.EntityManager;
+import javax.persistence.Persistence;
+import java.sql.SQLException;
+import java.util.logging.Logger;
+
 @DataConfiguration
-public class H2DataBaseConfiguration {
+public final class H2DataBaseConfiguration {
 
+    private static final Logger logger = Logger.getLogger(H2DataBaseConfiguration.class.getName());
+    private static Server server;
+    private static final String PERSISTENCE_UNIT_NAME = "h2config";
+    private final EntityManager entityManager;
+    private static volatile H2DataBaseConfiguration instance = null;
 
+    private H2DataBaseConfiguration() {
+        entityManager = Persistence.createEntityManagerFactory("h2config")
+                .createEntityManager();
+    }
+
+    public static H2DataBaseConfiguration getDataBaseInstance() {
+        if (instance != null) {
+            return instance;
+        } else {
+            synchronized (H2DataBaseConfiguration.class) {
+                if (instance == null) {
+                    instance = new H2DataBaseConfiguration();
+                }
+                return instance;
+            }
+        }
+    }
+
+    public EntityManager getEntityManager() {
+        return this.entityManager;
+    }
+
+    /*
+     * Starts the database TCP server in case one needs to access it using a 3rd party(external) client
+     *
+     * */
+    public static void startDatabase() throws SQLException {
+        server = Server.createTcpServer()
+                .start();
+    }
+
+    /*
+     * Stops the database server
+     *
+     * */
+    public static void stopDatabase() {
+        server.stop();
+    }
 }
---
+++ employee-service/src/main/java/org/jadevirek/data/model/Employee.java
@@ -0,0 +1,6 @@
+package org.jadevirek.data.model;
+
+public class Employee {
+
+
+}
